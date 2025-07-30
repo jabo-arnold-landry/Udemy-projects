d6a6e4cee71d9a96cc94ad564742109c018d5890
jabo arnold landry <arnoldjab@gmail.com>
2025-07-30 14:41:00 +0200

    feat:adding-click-highligt

--- javascript/audioPlayer.js
+++ javascript/audioPlayer.js
@@ -1,6 +1,7 @@
 const audios = document.getElementsByTagName("audio");
-const songsList = document.getElementsByTagName("li");
-
+const songTitle = document.getElementsByTagName("li");
+const songContainer = document.querySelector("ul");
+let currAudioIndex = null;
 function playerController(cntrl) {
   return function (index) {
     audios[index].pause();
@@ -10,16 +11,32 @@ function playerController(cntrl) {
 }
 function playing(index = 0) {
   audios[index].play();
+  currAudioIndex = index;
+  document.querySelector("#play").innerText = "pause";
   highlightPlayingSong(audios[index].dataset.count);
 }
 
 function pause(index) {
   audios[index].pause();
+  document.querySelector("#play").innerText = "play";
 }
 function highlightPlayingSong(la) {
-  for (let tag of songsList) {
+  for (let tag of songTitle) {
     tag.classList.remove("active-song");
   }
-  songsList[la].classList.toggle("active-song");
+  songTitle[la].classList.toggle("active-song");
 }
+songContainer.addEventListener("click", (e) => {
+  highlightPlayingSong(e.target.dataset.label);
+});
+// songContainer.addEventListener("dblclick", (e) => {
+//   if (e.target.matches("li")) {
+//     if (currAudioIndex !== null && currAudioIndex !== e.target.dataset.label) {
+//       audios[currAudioIndex].pause();
+//       audios[currAudioIndex].currentTime = 0;
+//     }
+//     playing(e.target.dataset.label);
+//     currAudioIndex = e.target.label;
+//   }
+// });
 export { playerController, playing, pause };
